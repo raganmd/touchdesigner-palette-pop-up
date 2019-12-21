2ecb7200eef1ee2295e76d4bae309914b4023073
Matthew Ragan <raganmd@gmail.com>
2019-12-20 18:28:56 -0800

    Extension Updates
    successfully adds mechanism for finding TOX path based on clicked row item in UI

--- td-modules/base_palette/paletteEXT.py
+++ td-modules/base_palette/paletteEXT.py
@@ -1,8 +1,10 @@
 
 class PaletteExplorer:
 	def __init__(self, myop):
-		self.MyOp = myop
-		self.PaletteWindowCOMP = op('window_palette')
+		self.MyOp 					= myop
+		self.PaletteWindowCOMP 		= op('window_palette')
+		self.SelectContainer 		= op('container_palette_widget/container_body/container_selected_palette')
+		self.LastSelectedToxPath 	= tdu.Dependency(None)
 		pass
 
 	def PaletteWindow(self, state):
@@ -14,3 +16,13 @@ class PaletteExplorer:
 			self.PaletteWindowCOMP.par.winclose.pulse()
 
 		pass
+	
+	def FindToxToLoad(self, info):
+		row 			= info.get('row')
+		lister 			= info.get('ownerComp').parent(2)
+		folderDAT 		= info.get('ownerComp').parent(3).op('folder1')
+		folderRowIndex 	= (row+1) + ((lister.digits - 1) * self.SelectContainer.par.Numrows)
+
+		TOXPath 		= folderDAT[ folderRowIndex, 'path']
+
+		self.LastSelectedToxPath.val = TOXPath
